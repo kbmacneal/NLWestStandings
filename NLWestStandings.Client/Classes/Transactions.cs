ec4cd21458cad07cb1f507d58f4ac6c039022026
Ken MacNeal <kenneth.b.macneal@gmail.com>
2024-08-16 22:29:53 -0500

    transations done

---
+++ NLWestStandings.Client/Classes/Transactions.cs
@@ -0,0 +1,45 @@
+﻿namespace NLWestStandings.Client.Classes.Transactions
+{
+
+    public class Transactions
+    {
+        public string copyright { get; set; }
+        public Transaction[] transactions { get; set; }
+    }
+
+    public class Transaction
+    {
+        public int id { get; set; }
+        public Person person { get; set; }
+        public Toteam toTeam { get; set; }
+        public string date { get; set; }
+        public string effectiveDate { get; set; }
+        public string resolutionDate { get; set; }
+        public string typeCode { get; set; }
+        public string typeDesc { get; set; }
+        public string description { get; set; }
+        public Fromteam fromTeam { get; set; }
+    }
+
+    public class Person
+    {
+        public int id { get; set; }
+        public string fullName { get; set; }
+        public string link { get; set; }
+    }
+
+    public class Toteam
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+    }
+
+    public class Fromteam
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+    }
+
+}
