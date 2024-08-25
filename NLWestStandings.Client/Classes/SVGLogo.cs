aff24553da38a0eaae26898723203a8e62062530
Ken MacNeal <kenneth.b.macneal@gmail.com>
2024-08-25 12:00:23 -0500

    adding darkmode images

--- NLWestStandings.Client/Classes/SVGLogo.cs
+++ NLWestStandings.Client/Classes/SVGLogo.cs
@@ -2,48 +2,95 @@
 {
     public static class SVGLogo
     {
-        public static string GetLogo(string? team_name)
+        public static string GetLogo(string? team_name, bool dark_mode=false)
         {
-            try
+            if(dark_mode)
             {
-                return team_name switch
+                try
                 {
-                    "Arizona Diamondbacks" => "ari_l.svg",
-                    "Atlanta Braves" => "atl_l.svg",
-                    "Baltimore Orioles" => "bal_l.svg",
-                    "Boston Red Sox" => "bos_l.svg",
-                    "Chicago Cubs" => "chc_l.svg",
-                    "Chicago White Sox" => "cws_l.svg",
-                    "Cincinnati Reds" => "cin_l.svg",
-                    "Cleveland Guardians" => "cle_l.svg",
-                    "Colorado Rockies" => "col_l.svg",
-                    "Detroit Tigers" => "det_l.svg",
-                    "Houston Astros" => "hou_l.svg",
-                    "Kansas City Royals" => "kc_l.svg",
-                    "Los Angeles Angels" => "laa_l.svg",
-                    "Los Angeles Dodgers" => "lad_l.svg",
-                    "Miami Marlins" => "mia_l.svg",
-                    "Milwaukee Brewers" => "mil_l.svg",
-                    "Minnesota Twins" => "min_l.svg",
-                    "New York Mets" => "nym_l.svg",
-                    "New York Yankees" => "nyy_l.svg",
-                    "Oakland Athletics" => "oak_l.svg",
-                    "Philadelphia Phillies" => "phi_l.svg",
-                    "Pittsburgh Pirates" => "pit_l.svg",
-                    "San Diego Padres" => "sd_l.svg",
-                    "San Francisco Giants" => "sf_l.svg",
-                    "Seattle Mariners" => "sea_l.svg",
-                    "St. Louis Cardinals" => "stl_l.svg",
-                    "Tampa Bay Rays" => "tb_l.svg",
-                    "Texas Rangers" => "tex_l.svg",
-                    "Toronto Blue Jays" => "tor_l.svg",
-                    "Washington Nationals" => "wsh_l.svg",
-                    _ => "",
-                };
+                    return team_name switch
+                    {
+                        "Arizona Diamondbacks" => "ari_d.svg",
+                        "Atlanta Braves" => "atl_d.svg",
+                        "Baltimore Orioles" => "bal_d.svg",
+                        "Boston Red Sox" => "bos_d.svg",
+                        "Chicago Cubs" => "chc_d.svg",
+                        "Chicago White Sox" => "cws_d.svg",
+                        "Cincinnati Reds" => "cin_d.svg",
+                        "Cleveland Guardians" => "cle_d.svg",
+                        "Colorado Rockies" => "col_d.svg",
+                        "Detroit Tigers" => "det_d.svg",
+                        "Houston Astros" => "hou_d.svg",
+                        "Kansas City Royals" => "kc_d.svg",
+                        "Los Angeles Angels" => "laa_d.svg",
+                        "Los Angeles Dodgers" => "lad_d.svg",
+                        "Miami Marlins" => "mia_d.svg",
+                        "Milwaukee Brewers" => "mil_d.svg",
+                        "Minnesota Twins" => "min_d.svg",
+                        "New York Mets" => "nym_d.svg",
+                        "New York Yankees" => "nyy_d.svg",
+                        "Oakland Athletics" => "oak_d.svg",
+                        "Philadelphia Phillies" => "phi_d.svg",
+                        "Pittsburgh Pirates" => "pit_d.svg",
+                        "San Diego Padres" => "sd_d.svg",
+                        "San Francisco Giants" => "sf_d.svg",
+                        "Seattle Mariners" => "sea_d.svg",
+                        "St. Louis Cardinals" => "stl_d.svg",
+                        "Tampa Bay Rays" => "tb_d.svg",
+                        "Texas Rangers" => "tex_d.svg",
+                        "Toronto Blue Jays" => "tor_d.svg",
+                        "Washington Nationals" => "wsh_d.svg",
+                        _ => "",
+                    };
+                }
+                catch (Exception)
+                {
+                    return "";
+                }
             }
-            catch (Exception)
+            else
             {
-                return "";
+                try
+                {
+                    return team_name switch
+                    {
+                        "Arizona Diamondbacks" => "ari_l.svg",
+                        "Atlanta Braves" => "atl_l.svg",
+                        "Baltimore Orioles" => "bal_l.svg",
+                        "Boston Red Sox" => "bos_l.svg",
+                        "Chicago Cubs" => "chc_l.svg",
+                        "Chicago White Sox" => "cws_l.svg",
+                        "Cincinnati Reds" => "cin_l.svg",
+                        "Cleveland Guardians" => "cle_l.svg",
+                        "Colorado Rockies" => "col_l.svg",
+                        "Detroit Tigers" => "det_l.svg",
+                        "Houston Astros" => "hou_l.svg",
+                        "Kansas City Royals" => "kc_l.svg",
+                        "Los Angeles Angels" => "laa_l.svg",
+                        "Los Angeles Dodgers" => "lad_l.svg",
+                        "Miami Marlins" => "mia_l.svg",
+                        "Milwaukee Brewers" => "mil_l.svg",
+                        "Minnesota Twins" => "min_l.svg",
+                        "New York Mets" => "nym_l.svg",
+                        "New York Yankees" => "nyy_l.svg",
+                        "Oakland Athletics" => "oak_l.svg",
+                        "Philadelphia Phillies" => "phi_l.svg",
+                        "Pittsburgh Pirates" => "pit_l.svg",
+                        "San Diego Padres" => "sd_l.svg",
+                        "San Francisco Giants" => "sf_l.svg",
+                        "Seattle Mariners" => "sea_l.svg",
+                        "St. Louis Cardinals" => "stl_l.svg",
+                        "Tampa Bay Rays" => "tb_l.svg",
+                        "Texas Rangers" => "tex_l.svg",
+                        "Toronto Blue Jays" => "tor_l.svg",
+                        "Washington Nationals" => "wsh_l.svg",
+                        _ => "",
+                    };
+                }
+                catch (Exception)
+                {
+                    return "";
+                }
             }
             
         }
--- NLWestStandings.Client/Program.cs
+++ NLWestStandings.Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using NLWestStandings.Client.Classes;
@@ -7,6 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 //builder.Services.AddSingleton<Logos>(System.Text.Json.JsonSerializer.Deserialize<Logos>(File.ReadAllText(System.IO.Path.Combine("wwwroot", "logos.json"))));
+
+builder.Services.AddBlazoredLocalStorageAsSingleton();
 
 builder.Services.AddMudServices(options =>
 {
